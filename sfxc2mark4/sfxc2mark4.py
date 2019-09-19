bcc7695727f3517950300d3a600e5d5b99338b33
keimpema <keimpema@5736e3ea-d426-0410-8aaf-93e336ac3372>
2019-09-19 11:28:53 +0000

    Make it possible to supply the one letter codes using a JSON file.
    git-svn-id: svn+ssh://code.jive.nl/code/svn/sfxc/trunk@2222 5736e3ea-d426-0410-8aaf-93e336ac3372

--- sfxc2mark4/sfxc2mark4.py
+++ sfxc2mark4/sfxc2mark4.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
-
 import create_type1
 import create_type3
+import stationmap
 
-vex, ctrl, rootid, ovexfile = create_type1.parse_args()
+vex, ctrl, rootid, ovexfile, codefile = create_type1.parse_args()
+stationmap.create_one_letter_mapping(vex, codefile)
 create_type1.process_job(vex, ctrl, rootid, ovexfile)
 create_type3.process_job(vex, ctrl, rootid)
